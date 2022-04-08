856678354277329cb0bb978f93d9a8baf7b60c1e
Kris <kristhecanadian101@gmail.com>
2022-04-08 16:00:51 -0400

    added view in nav

--- app/src/main/java/me/kristhecanadian/gooutside/ui/ar/ArFragment.kt
+++ app/src/main/java/me/kristhecanadian/gooutside/ui/ar/ArFragment.kt
@@ -1,20 +1,16 @@
-package me.kristhecanadian.gooutside.ui.notifications
+package me.kristhecanadian.gooutside.ui.ar
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import me.kristhecanadian.gooutside.databinding.FragmentNotificationsBinding
+import me.kristhecanadian.gooutside.databinding.FragmentArBinding
 
-class NotificationsFragment : Fragment() {
+class ArFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentArBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -23,20 +19,20 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this)[ArViewModel::class.java]
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentArBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+//        val textView: TextView = binding.textNotifications
         notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+//            textView.text = it
         }
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+//        _binding = null
     }
 }
--- app/src/main/java/me/kristhecanadian/gooutside/ui/ar/ArViewModel.kt
+++ app/src/main/java/me/kristhecanadian/gooutside/ui/ar/ArViewModel.kt
@@ -1,10 +1,10 @@
-package me.kristhecanadian.gooutside.ui.notifications
+package me.kristhecanadian.gooutside.ui.ar
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ArViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
