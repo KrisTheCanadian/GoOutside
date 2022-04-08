51a6505e4389d2d8d3dc563325b72b2be858a0fe
Kris <kristhecanadian101@gmail.com>
2022-04-08 16:59:18 -0400

    converted AR_Activity to AR_Fragment

--- app/src/main/java/me/kristhecanadian/gooutside/ui/ar/ArFragment.kt
+++ app/src/main/java/me/kristhecanadian/gooutside/ui/ar/ArFragment.kt
@@ -1,14 +1,65 @@
 package me.kristhecanadian.gooutside.ui.ar
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.ar.sceneform.AnchorNode
+import me.kristhecanadian.gooutside.R
+import me.kristhecanadian.gooutside.api.NearbyPlacesResponse
+import me.kristhecanadian.gooutside.api.PlacesService
+import me.kristhecanadian.gooutside.ar.PlaceNode
+import me.kristhecanadian.gooutside.ar.PlacesArFragment
 import me.kristhecanadian.gooutside.databinding.FragmentArBinding
+import me.kristhecanadian.gooutside.model.Place
+import me.kristhecanadian.gooutside.model.getPositionVector
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class ArFragment : Fragment() {
+class ArFragment : Fragment(), SensorEventListener {
+
+    private val TAG = "AR_Activity"
+
+    private lateinit var placesService: PlacesService
+    private lateinit var arFragment: PlacesArFragment
+    private lateinit var mapFragment: SupportMapFragment
+
+    // Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    // Sensor
+    private lateinit var sensorManager: SensorManager
+    private val accelerometerReading = FloatArray(3)
+    private val magnetometerReading = FloatArray(3)
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
+
+    private var anchorNode: AnchorNode? = null
+    private var markers: MutableList<Marker> = emptyList<Marker>().toMutableList()
+    private var places: List<Place>? = null
+    private var currentLocation: Location? = null
+    private var map: GoogleMap? = null
 
     private var _binding: FragmentArBinding? = null
     private val binding get() = _binding!!
@@ -18,21 +69,266 @@ class ArFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this)[ArViewModel::class.java]
-
-        _binding = FragmentArBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+//        val notificationsViewModel =
+//            ViewModelProvider(this)[ArViewModel::class.java]
+//
+//        _binding = FragmentArBinding.inflate(inflater, container, false)
+//        val root: View = binding.root
 
 //        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
+//        notificationsViewModel.text.observe(viewLifecycleOwner) {
+////            textView.text = it
+//        }
+
+//        if (!isSupportedDevice()) {
+//            return
+//        }
+        val viewOfLayout = inflater.inflate(R.layout.fragment_ar, container, false)
+
+        // AR PANE
+        arFragment = childFragmentManager.findFragmentById(R.id.ar_fragment) as PlacesArFragment
+        // MAP
+        mapFragment =
+            childFragmentManager.findFragmentById(R.id.maps_fragment) as SupportMapFragment
+
+        sensorManager =
+            (requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager)
+        placesService = PlacesService.create()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+
+        setUpAr()
+        setUpMaps()
+
+
+        return viewOfLayout
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
         }
-        return root
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    private fun setUpAr() {
+        arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+            val anchor = hitResult.createAnchor()
+            anchorNode = AnchorNode(anchor)
+            anchorNode?.setParent(arFragment.arSceneView.scene)
+            addPlaces(anchorNode!!)
+        }
+    }
+
+    private fun addPlaces(anchorNode: AnchorNode) {
+        val currentLocation = currentLocation
+        if (currentLocation == null) {
+            Log.w(TAG, "Location has not been determined yet")
+            return
+        }
+
+        val places = places
+        if (places == null) {
+            Log.w(TAG, "No places to put")
+            return
+        }
+
+        for (place in places) {
+            // Add the place in AR
+            val placeNode = PlaceNode(requireActivity().applicationContext, place)
+            placeNode.setParent(anchorNode)
+            placeNode.localPosition = place.getPositionVector(orientationAngles[0], currentLocation.latLng)
+            placeNode.setOnTapListener { _, _ ->
+                showInfoWindow(place)
+            }
+
+
+            // Add the place in maps
+            map?.let {
+                val marker = it.addMarker(
+                    MarkerOptions()
+                        .position(place.geometry.location.latLng)
+                        .title(place.name)
+                )
+                marker?.tag = place
+                if (marker != null) {
+                    markers.add(marker)
+                }
+            }
+        }
+    }
+
+    private fun showInfoWindow(place: Place) {
+        // Show in AR
+        val matchingPlaceNode = anchorNode?.children?.filterIsInstance<PlaceNode>()?.first {
+            val otherPlace = (it as PlaceNode).place ?: return@first false
+            return@first otherPlace == place
+        } as? PlaceNode
+        matchingPlaceNode?.showInfoWindow()
+
+        // Show as marker
+        val matchingMarker = markers.firstOrNull {
+            val placeTag = (it.tag as? Place) ?: return@firstOrNull false
+            return@firstOrNull placeTag == place
+        }
+        matchingMarker?.showInfoWindow()
+    }
+
+    private fun setUpMaps() {
+        mapFragment.getMapAsync { googleMap ->
+            if (ActivityCompat.checkSelfPermission(
+                    requireActivity().applicationContext,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireActivity().applicationContext,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return@getMapAsync
+            }
+            googleMap.isMyLocationEnabled = true
+
+            getCurrentLocation {
+                val pos = CameraPosition.fromLatLngZoom(it.latLng, 13f)
+                googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(pos))
+                getNearbyPlaces(it)
+            }
+            googleMap.setOnMarkerClickListener { marker ->
+                val tag = marker.tag
+                if (tag !is Place) {
+                    return@setOnMarkerClickListener false
+                }
+                showInfoWindow(tag)
+                return@setOnMarkerClickListener true
+            }
+            map = googleMap
+        }
+    }
+
+    private fun getCurrentLocation(onSuccess: (Location) -> Unit) {
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity().applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity().applicationContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            currentLocation = location
+            onSuccess(location)
+        }.addOnFailureListener {
+            Log.e(TAG, "Could not get location")
+        }
+    }
+
+    private fun getNearbyPlaces(location: Location) {
+        // TODO CHANGE THESES PLACES FROM GOOGLE MAPS TO FIREBASE LOCATIONS
+        val apiKey = resources.getString(R.string.googleMapApiKey)
+        placesService.nearbyPlaces(
+            apiKey = apiKey,
+            location = "${location.latitude},${location.longitude}",
+            radiusInMeters = 200,
+            placeType = "park"
+        ).enqueue(
+            object : Callback<NearbyPlacesResponse> {
+                override fun onFailure(call: Call<NearbyPlacesResponse>, t: Throwable) {
+                    Log.e(TAG, "Failed to get nearby places", t)
+                }
+
+                override fun onResponse(
+                    call: Call<NearbyPlacesResponse>,
+                    response: Response<NearbyPlacesResponse>
+                ) {
+                    if (!response.isSuccessful) {
+                        Log.e(TAG, "Failed to get nearby places")
+                        return
+                    }
+
+                    val places = response.body()?.results ?: emptyList()
+                    this@ArFragment.places = places
+                }
+            }
+        )
+    }
+
+//    private fun isSupportedDevice(): Boolean {
+//        val availability = ArCoreApk.getInstance().checkAvailability(this)
+//        if (!availability.isSupported) {
+//            return false
+//        }
+//        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+//        val openGlVersionString = activityManager.deviceConfigurationInfo.glEsVersion
+//        if (openGlVersionString.toDouble() < 3.0) {
+//            Toast.makeText(this, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+//                .show()
+//            finish()
+//            return false
+//        }
+//        return true
+//    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event == null) {
+            return
+        }
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+        }
+
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(
+            rotationMatrix,
+            null,
+            accelerometerReading,
+            magnetometerReading
+        )
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-//        _binding = null
+        _binding = null
     }
 }
+
+val Location.latLng: LatLng
+    get() = LatLng(this.latitude, this.longitude)
